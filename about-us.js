25668e66c5a39e04bbc4fafc93d68fa6aa1b3191
lucasfraulin <lucasfraulingit@gmail.com>
2020-08-11 11:31:27 -0600

    added underlines to some of the headings looks really good

---
+++ about-us.js
@@ -0,0 +1,79 @@
+// var $window = $(window);
+// var $test = $('.test');
+// var breakpoint = 992;
+// var last = $window.width() > breakpoint;
+// $window.on('resize', function(){
+//   var wWwidth = $window.width();
+//   var isLarger = wWwidth > breakpoint
+//
+//   if(last !== isLarger) {
+//     if(isLarger) {
+//       onLargerChange();
+//     } else {
+//       onSmallerChange();
+//     }
+//     last = isLarger;
+//   }
+// });
+//
+// $window.on('resize', function(){
+//   var wWwidth = $window.width();
+//   var isLarger = wWwidth > breakpoint
+//
+//   if(last !== isLarger) {
+//     if(isLarger) {
+//       onLargerChange();
+//     } else {
+//       onSmallerChange();
+//     }
+//     last = isLarger;
+//   }
+// });
+
+function onSmallerChange() {
+  $("#about-carousel-container").removeClass("container");
+  $("#about-carousel").removeClass("about-carousel");
+  $("#about-carousel").removeClass("clearfix");
+  $("#about-carousel img").removeClass("img-responsive");
+  $("#about-carousel-container").addClass("container-fluid");
+  $("#about-carousel").addClass("about-carousel-fluid");
+  $("#about-carousel img").addClass("img-fluid w-100 about-car-img");
+}
+
+function onLargerChange() {
+  $("#about-carousel-container").removeClass("container-fluid");
+  $("#about-carousel").removeClass("about-carousel-fluid");
+  $("#about-carousel img").removeClass("img-fluid w-100 about-car-img");
+  $("#about-carousel img").addClass("img-responsive");
+  $("#about-carousel").addClass("about-carousel");
+  $("#about-carousel").addClass("clearfix");
+  $("#about-carousel-container").addClass("container");
+}
+// define the boundries
+var bounds = [
+    {min:0,max:992,func:onSmallerChange},
+    {min:993,func:onLargerChange}
+];
+
+// define a resize function. use a closure for the lastBoundry determined.
+var resizeFn = function(){
+    var lastBoundry; // cache the last boundry used
+    return function(){
+        var width = window.innerWidth; // get the window's inner width
+        var boundry, min, max;
+        for(var i=0; i<bounds.length; i++){
+            boundry = bounds[i];
+            min = boundry.min || Number.MIN_VALUE;
+            max = boundry.max || Number.MAX_VALUE;
+            if(width > min && width < max
+               && lastBoundry !== boundry){
+                lastBoundry = boundry;
+                return boundry.func.call(boundry);
+            }
+        }
+    }
+};
+$(window).resize(resizeFn()); // bind the resize event handler
+$(document).ready(function(){
+    $(window).trigger('resize'); // on load, init the lastBoundry
+});
